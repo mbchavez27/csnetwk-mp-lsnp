6a6e77e4644114771b8f672dc24ad9755363be15
EvanLicup <evanlicup12@gmail.com>
2025-08-03 15:26:59 +0800

    Update post.py

--- handlers/post.py
+++ handlers/post.py
@@ -13,3 +13,31 @@
 #
 # NOTE: use (if logger and logger.verbose:) for conditional verbose logging
 # add your commands in commands.py and call handlers in dispatcher.py
+
+import time
+import secrets
+
+def build_post_message(user_id: str, content: str, ttl=3600):
+    timestamp = int(time.time())
+    token = f"{user_id}|{timestamp + ttl}|broadcast"
+    message_id = secrets.token_hex(8)
+
+    return f"""TYPE: POST
+USER_ID: {user_id}
+CONTENT: {content}
+TTL: {ttl}
+MESSAGE_ID: {message_id}
+TOKEN: {token}
+
+"""
+
+def  handle_post(message: dict, peer_table, logger=None):
+    user_id = message.get("USER_ID")
+    content = message.get("CONTENT")
+    display_name = peer_table.get_display_name(user_id)
+
+    name_to_show = display_name if display_name is not None else user_id
+    print(f"{name_to_show}:{content}")
+
+    if logger and logger.verbose:
+        logger.debug(f"[VERBOSE LOG] Received POST:\n{message}")
